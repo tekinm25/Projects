9d30ac223e878a3e9829ca478ca1118b58b752fa
tekinm25 <tekinm25@users.noreply.github.com>
2015-01-07 23:04:12 -0500

    Pi Project update
    Fixed style inconsistencies

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -7,20 +7,20 @@ from math import pi
 # Doing it this way allows you to generate pi to the desired
 # decimal place (below 12) without having the last digit rounded
 def precision():
-	print("How many decimal places of pi do you want to see?")
-	a = raw_input('> ')
-	try:
-		if int(a) > 0 and int(a) < 12:
-			x = int(a)
-			y = pi * (10 ** x)
-			z = int(y)
-			b = z / float(10 ** int(x))
-			print b
-		elif int(a) <= 0 or int(a) >= 12:
-			print("Error: Input must be a positive integer and less than 12")
-			precision()
-	except ValueError:
-		print("ValueError: Input be an integer")
-		precision()
-	
+    print("How many decimal places of pi do you want to see?")
+    a = raw_input('> ')
+    try:
+        if int(a) > 0 and int(a) < 12:
+            x = int(a)
+            y = pi * (10 ** x)
+            z = int(y)
+            b = z / float(10 ** int(x))
+            print b
+        elif int(a) <= 0 or int(a) >= 12:
+            print("Error: Input must be a positive integer and less than 12")
+            precision()
+    except ValueError:
+        print("ValueError: Input be an integer")
+        precision()
+
 precision()
